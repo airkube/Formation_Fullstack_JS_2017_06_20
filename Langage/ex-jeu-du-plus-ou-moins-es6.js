7d0fbd0efbcde97e4c6b89701c49e9df2a03b7b0
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-06-21 10:50:40 +0100

    Correction ES6

--- Langage/ex-jeu-du-plus-ou-moins-es6.js
+++ Langage/ex-jeu-du-plus-ou-moins-es6.js
@@ -3,18 +3,18 @@ const readline = require('readline');
 // 1 - Utiliser dans Random les Methods Properties
 // http://es6-features.org/#MethodProperties
 const Random = {
-  get: function () {
+  get() {
     return Math.random();
   },
-  getArbitrary: function (min, max) {
+  getArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   },
-  getInt: function (min, max) {
+  getInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
   },
-  getIntInclusive: function (min, max) {
+  getIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
@@ -23,60 +23,59 @@ const Random = {
 
 // 2 - Utiliser le mot clé class
 // http://es6-features.org/#ClassDefinition
-const Jeu = function(options) {
-  // 3 - Utiliser les valeurs par défaut
-  // http://es6-features.org/#DefaultParameterValues
-  options = options || {};
-  // 4 - Destructurer options avec des valeurs par défaut
-  // http://es6-features.org/#ObjectAndArrayMatchingDefaultValues
-  const min = options.min || 0;
-  const max = (options.max !== undefined) ? options.max : 100;
-  this._rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+class Jeu {
+  constructor(options = {}) {
+    // 3 - Utiliser les valeurs par défaut
+    // http://es6-features.org/#DefaultParameterValues
+    // options = options || {};
+    // 4 - Destructurer options avec des valeurs par défaut
+    // http://es6-features.org/#ObjectAndArrayMatchingDefaultValues
+    const {min = 0, max = 100} = options;
+    this._rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-  this._entierAlea = Random.getIntInclusive(min, max);
-  this._essais = [];
-};
-
-Jeu.prototype.jouer = function() {
-  if (this._essais.length) {
-    // 5 - Utiliser une template string
-    // http://es6-features.org/#StringInterpolation
-    console.log('Vous avez déjà joué : ' + this._essais.join(' - '));
+    this._entierAlea = Random.getIntInclusive(min, max);
+    this._essais = [];
   }
 
-  this._rl.question('Saisir un entier : ', (answer) => {
-
-    // 6 - Passer par l'objet Number pour parseInt
-    // et isNaN
-    // http://es6-features.org/#NumberTypeChecking
-    const entierSaisi = parseInt(answer);
-
-    if (isNaN(entierSaisi)) {
-      console.log('Erreur : il faut saisir un nombre');
-      return this.jouer();
+  jouer() {
+    if (this._essais.length) {
+      // 5 - Utiliser une template string
+      // http://es6-features.org/#StringInterpolation
+      console.log(`Vous avez déjà joué : ${this._essais.join(' - ')}`);
     }
 
-    this._essais.push(entierSaisi);
+    this._rl.question('Saisir un entier : ', (answer) => {
 
-    if (entierSaisi < this._entierAlea) {
-      console.log(entierSaisi + ' est trop petit');
-      return this.jouer();
-    }
+      // 6 - Passer par l'objet Number pour parseInt
+      // et isNaN
+      // http://es6-features.org/#NumberTypeChecking
+      const entierSaisi = Number.parseInt(answer);
 
-    if (entierSaisi > this._entierAlea) {
-      console.log(entierSaisi + ' est trop grand');
-      return this.jouer();
-    }
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Erreur : il faut saisir un nombre');
+        return this.jouer();
+      }
 
-    console.log('Gagné');
-    this._rl.close();
-  });
-};
+      this._essais.push(entierSaisi);
 
+      if (entierSaisi < this._entierAlea) {
+        console.log(entierSaisi + ' est trop petit');
+        return this.jouer();
+      }
+
+      if (entierSaisi > this._entierAlea) {
+        console.log(entierSaisi + ' est trop grand');
+        return this.jouer();
+      }
+
+      console.log('Gagné');
+      this._rl.close();
+    });
+  }
+}
 
 const jeu = new Jeu();
-
 jeu.jouer();
